17a009f1b2f18a8ae9f541fa84a67d763a174896
Samir1984 <allandanielito1984@gmail.com>
2026-07-10 19:00:33 -0600

    Implementación de menu funcional con estructura base con cierre controlado

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,47 @@ use std::io;
 use std::io::Write;
 
 fn main() {
-    print!();
+    loop {
+        println!("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        println!("-_-          ¡Bienvenido al sistema!          -_-");
+        println!("-_-             Calculadora Rust              -_-");
+        println!("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        println!();
+        println!("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        println!("-_-               MENÚ PRINCIPAL              -_-");
+        println!("-_-                1. Sumar                   -_-");
+        println!("-_-                2. Restar                  -_-");
+        println!("-_-                3. Multiplicar             -_-");  
+        println!("-_-                4. Dividir                 -_-");
+        println!("-_-                5. Salir                   -_-");
+        println!("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        println!();
+        print!("Por favor, selecciona tu operación: ");
+        let mut entrada:String=String::new();
+        io::stdout().flush().expect("Error en el forzamiento del buffer.");
+        io::stdin().read_line(&mut entrada).expect("Error en la lectura de la línea.");
+        let opcion:u64=entrada.trim().parse().expect("Error en la conversión de datos.");
+        match opcion{
+            1=>{
+
+            },
+            2=>{
+
+            },
+            3=>{
+
+            },
+            4=>{
+
+            },
+            5=>{
+                println!("Saliendo del programa..."); break;
+            },
+            _=>{
+                println!("Opción inválida, por favor, ingrese una opción correcta: ");
+                println!();
+            }
+        }
+
+    }
 }
